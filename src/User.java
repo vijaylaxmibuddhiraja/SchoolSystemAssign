6d6c6bbaf01d5bd6185d7d922f869a9c3aed7fd8
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2024-10-14 20:54:01 +0200

    created admin.java class, superclass User made by abstract, Admin subclass using extends

--- src/User.java
+++ src/User.java
@@ -1,2 +1,29 @@
-public class User {
+import java.util.ArrayList;
+
+
+abstract class User {
+    private final String username;
+    private String password;
 }
+
+public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+}
+
+public String getUsername() {
+    return username;
+}
+
+public boolean checkpassword(String password) {
+    return this.password.equals(password);
+}
+
+public void setPassword(String newPassword) {
+    this.password = newPassword;
+}
+
+public abstract void showMenu();
+public abstract void actionHandling(int choice, SchoolSystem system);
+}
+
