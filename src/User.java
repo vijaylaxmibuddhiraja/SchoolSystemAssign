7d51238306352c86f0122e1012e54983ec6009bb
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2024-10-15 22:51:32 +0200

    User class made abstract

--- src/User.java
+++ src/User.java
@@ -1,6 +1,39 @@
-public class User {
+abstract class User {                   // making the class as a superclass
+    private String username;            //encapsulation
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;    //constructor
+    }
+
+    // getter and setter method
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.equals(password);
+    }
+
+    public abstract void menu();              // abstract method
+    public abstract void viewInformation();
 
 }
+
 
 
 
