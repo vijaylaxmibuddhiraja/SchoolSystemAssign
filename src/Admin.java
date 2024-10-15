b85a17873d5fd0a053641b0667022eaa2eea53e3
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2024-10-15 19:30:12 +0200

    structure codes removed due to errors

--- src/Admin.java
+++ src/Admin.java
@@ -1,35 +1,3 @@
-public class Admin extends User{
-    public Admin(String username, String password){
-        super(username, password);
-    }
+public class Admin {
 
-    @override
-     public void showMenu(){
-        System.out.println("1. Add new user");
-        System.out.println("2. Change password");
-        System.out.println("3. Remove user");
-        System.out.println("4.Logout");
-    }
-
-    @Override actionHandling(int choice, SchoolSystem system){
-        switch (choice) {
-            case 1:
-                system.addNewUser(); // Admin-specific functionality
-                break;
-            case 2:
-                system.changePassword(this); // Admin-specific functionality
-                break;
-            case 3:
-                system.removeUser(); // Admin-specific functionality
-                break;
-            case 4:
-                System.out.println("Logging out...");
-                break;
-            default:
-                System.out.println("Invalid choice.");
-        }
-    }
 }
-
-
-
--- src/User.java
+++ src/User.java
@@ -1,29 +1,6 @@
-import java.util.ArrayList;
+public class User {
 
-
-abstract class User {
-    private final String username;
-    private String password;
 }
 
-public User(String username, String password) {
-    this.username = username;
-    this.password = password;
-}
 
-public String getUsername() {
-    return username;
-}
-
-public boolean checkpassword(String password) {
-    return this.password.equals(password);
-}
-
-public void setPassword(String newPassword) {
-    this.password = newPassword;
-}
-
-public abstract void showMenu();
-public abstract void actionHandling(int choice, SchoolSystem system);
-}
 
