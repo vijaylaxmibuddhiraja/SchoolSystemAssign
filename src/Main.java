9b0894708492e3d5bfe01a8b20c030f10b88589b
vijaylaxmibuddhiraja <vijaylaxmi.buddhiraja@gmail.com>
2024-10-16 13:41:36 +0200

    Main program created

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import javax.xml.transform.Source;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,11 +8,48 @@ public class Main {
             Admin admin = new Admin("Admin", "superbusy_2024");
             Teacher teacher = new Teacher("Stefi Graff", "Iamthebest");
             Student student = new Student ("Maria Sharapova", "whythiskolaveriD");
+            
+            ArrayList<User>users = new ArrayList<>();
+            users.add(admin);
+            users.add(teacher);
+            users.add(student);
 
-            admin.addUser(teacher);
-            admin.addUser(student);
+            while(true) {
+                System.out.println("Enter Username (or 'exit' to quit): ");
+                String username = scanner.nextLine();
 
+                if (username.equalsIgnoreCase("exit")) {
+                    break;
+                }
+                System.out.println("Enter the password: ");
+                String password = scanner.nextLine();
 
+                User loggedInUser = null;
 
+                for (User user : users) {
+                    if (user.getUsername().equals(username) && user.checkPassword(password)) {
+                        loggedInUser = user;
+                        break;
+                    }
+                }
+
+                if (loggedInUser != null) {
+                    System.out.println("Login as" + loggedInUser.getClass().getSimpleName());
+                    loggedInUser.showMenu();
+                    loggedInUser.viewInformation();
+                    System.out.println("Logout now!");
+                } else {
+                    System.out.println("Invalid Options..Please try again!");
+                }
+            }
+
+        System.out.println("Program ends");
+            scanner.close();
+        }
     }
-}
+
+
+
+
+
+
